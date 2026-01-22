b5012eb286167e3e7e84d9445f22a17e5c16b058
wmk961038-hash <wmk961038@gmail.com>
2026-01-22 16:20:35 +0800

    直接两层循环计算所有的情况，减去正方形的情况

--- P2241/P2241.cpp
+++ P2241/P2241.cpp
@@ -1,10 +1,5 @@
 #include <bits/stdc++.h>
 using namespace std;
-struct rectangle
-{
-    int length;
-    int wide;
-};
 int the_smaller_of(int a, int b)
 {
     return a > b ? b : a;
@@ -24,33 +19,15 @@ void swap(int *pa, int *pb)
     *pa = *pb;
     *pb = temp;
 }
-long long cal_rectangle(int a_small, int a_big)
+long long cal_every(int length, int wide)
 {
-    // 找出所有可以在长方形中存在的表示小长方形的数对
-    int count = 0;
-    if (a_small > a_big)
-        swap(&a_small, &a_big);
-    vector<rectangle> rectangles;
-    for (int b_small = 1; b_small <= a_small; b_small++)
+    int count=0;
+    for(int i=1;i<=length;i++)
     {
-        for (int b_big = b_small + 1; b_big <= a_big; b_big++)
+        for(int j=1;j<=wide;j++)
         {
-            rectangle temp_rec;
-            temp_rec.length = b_small;
-            temp_rec.wide = b_big;
-            rectangles.push_back(temp_rec);
-            if (b_big <= a_small && b_small <= a_big)
-            {
-                rectangle temp_rec;
-                temp_rec.length = b_big;
-                temp_rec.wide = b_small;
-                rectangles.push_back(temp_rec);
-            }
+            count+=(length-i+1)*(wide-j+1);
         }
-    }
-    for(int i=0;i<rectangles.size();i++)
-    {
-        count+=(a_small-rectangles[i].length+1) * (a_big-rectangles[i].wide+1);
     }
     return count;
 }
@@ -58,6 +35,6 @@ int main()
 {
     int a, b;
     cin >> a >> b;
-    cout << cal_square(a, b) << ' ' << cal_rectangle(a, b);
+    cout << cal_square(a, b) << ' ' << cal_every(a, b)-cal_square(a, b);
     return 0;
 }
